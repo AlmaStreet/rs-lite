ff3458b01355cc2512e1b7ddc3e1afd8cc5f52a8
Admin <tian@Tian-Pro.local>
2023-11-27 22:43:45 -0800

    Refactor afk menu and clicker

--- rs_afk_menu.py
+++ rs_afk_menu.py
@@ -5,81 +5,71 @@ from time import sleep
 from tkinter import IntVar, Label, Radiobutton, Tk, W
 
 
+def initialize_ui():
+    tk = Tk()
+    tk.title("AFK Selection")
+    tk.geometry("240x720")
+
+    return tk
+
+
 def set_values():
     selection = str(var.get())
-
     text = "You selected the option " + str(selection)
     now = datetime.now().strftime("%H:%M:%S")
 
     if selection in data:
-        print("[", now, f"] === {data[selection]['label']} ===")
-        click.misc_state = eval(data[selection]["misc_state"])
-        click.elven_state = eval(data[selection]["elven_state"])
-        click.click_state = eval(data[selection]["click_state"])
-
-        click.misc_time_random = data[selection]["misc_time_random"]
-        click.misc_time_constant = data[selection]["misc_time_constant"]
-
-        click.elven_time_random = data[selection]["elven_time_random"]
-        click.elven_time_constant = data[selection]["elven_time_constant"]
-
-        click.click_time_random = data[selection]["click_time_random"]
-        click.click_time_constant = data[selection]["click_time_constant"]
-
-        if "special" in data[selection]:
-            click.special = data[selection]["special"]
-
-        print(f"misc_state: {click.misc_state}")
-        print(f"elven_state: {click.elven_state}")
-        print(f"click_state: {click.click_state}")
-        print()
-        print(f"misc_time_random: {click.misc_time_random}")
-        print(f"misc_time_constant: {click.misc_time_constant}")
-        print()
-        print(f"elven_time_random: {click.elven_time_random}")
-        print(f"elven_time_constant: {click.elven_time_constant}")
-        print()
-        print(f"click_time_random: {click.click_time_random}")
-        print(f"click_time_constant: {click.click_time_constant}")
-
-        print(f"special: {click.special}")
+        click.update_data(data[selection])
 
         text_state = f"\nmisc: {click.misc_state}\nelven: {click.elven_state}\nclick: {click.click_state}\n== special: {click.special} =="
         label.config(text=text + text_state)
 
+        profile = (
+            f"[{now}]\n"
+            f"=== {data[selection]['label']} ===\n"
+            f"    misc_state: {click.misc_state}\n"
+            f"    elven_state: {click.elven_state}\n"
+            f"    click_state: {click.click_state}\n"
+            "\n"
+            f"    misc_time_random: {click.misc_time_random}\n"
+            f"    misc_time_constant: {click.misc_time_constant}\n"
+            "\n"
+            f"    elven_time_random: {click.elven_time_random}\n"
+            f"    elven_time_constant: {click.elven_time_constant}\n"
+            "\n"
+            f"    click_time_random: {click.click_time_random}\n"
+            f"    click_time_constant: {click.click_time_constant}\n"
+            "\n"
+            f"    special: {click.special}\n"
+        )
+        print(profile)
 
-click = Click()
-click.start()
 
-f = open("data.json")
-data = json.load(f)
+if __name__ == "__main__":
+    root = initialize_ui()
 
-root = Tk()
-root.title("AFK Selection")
-root.geometry("240x700")
+    click = Click()
+    click.start()
 
-var = IntVar(value=len(data))
+    with open("data.json") as file:
+        data = json.load(file)
 
-index = 1
-for element in data:
-    radio_button = Radiobutton(
-        root, text=data[element]["label"], variable=var, value=index, command=set_values
-    )
-    index += 1
-    radio_button.pack(anchor=W)
+    var = IntVar(value=len(data))
 
-label = Label(root)
-label.pack()
-root.update()
+    for index, element in enumerate(data):
+        radio_button = Radiobutton(
+            root,
+            text=data[element]["label"],
+            variable=var,
+            value=index + 1,
+            command=set_values,
+        )
+        radio_button.pack(anchor=W)
 
-while True:
-    sleep(0.1)
+    label = Label(root)
+    label.pack()
     root.update()
 
-    # print(f"click.state: {click.state}")
-    # print("fps: {}".format(1 / (time() - last_time)))
-
-    # Press "q" to quit
-    if 0xFF == ord("q"):
-        click.stop()
-        break
+    while True:
+        sleep(0.1)
+        root.update()
